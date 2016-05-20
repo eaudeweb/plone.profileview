584fb305a367db5621a53ed6016caf8f0766165f
Alin Voinea <alin@eaudeweb.ro>
2016-05-20 12:02:45 +0300

    Prepare release 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = open('version.txt').read().strip()
 setup(name='plone.profileview',
       version=version,
       description="BrowserView profiler",
-      long_description=open("README.md").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
